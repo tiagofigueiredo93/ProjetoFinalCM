699e3b3d1b5c50f22020e6772f10db0b564f4fbe
tiagofigueiredo93 <tiago.figueiredo.bcl@gmail.com>
2021-03-25 10:32:54 +0000

    Enviar projeto inicial para o repositório.

---
+++ app/src/main/java/ipvc/estg/projetofinal/MainActivity.kt
@@ -0,0 +1,13 @@
+package ipvc.estg.projetofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
